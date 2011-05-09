6ab9e9cf79e6123690d363dbfa6d285cf1d46c7a
bullfight <p.schmitz@gmail.com>
2011-05-09 09:40:05 +0200

    refactoring

---
+++ lib/knmi/station.rb
@@ -0,0 +1,211 @@
+class KNMI
+  include HTTParty
+  base_uri 'http://www.knmi.nl/climatology/daily_data/getdata_day.cgi'
+    
+  class << self
+    private :new
+    
+    # station1:station2:station15 requires station or list of stations NO DEFAULT
+    def station(station_number) 
+      if station_number.nil? == true
+        raise "Station Number Required" # doesn't work look into this
+      elsif station_number.kind_of?(Array)
+        "stns=#{station_number * ":"}"
+      else
+        "stns=#{station_number}"
+      end
+    end
+    
+    def check_stations
+      # stubbed out complete later
+    end
+    
+    # YYYYMMDD Default is first day of current month
+    def start_date(_start)
+      "start=#{_start}"
+    end
+    
+    # YYYYMMDD Default is current or last recorded day
+    def end_date(_end)
+      "end=#{_end}"
+    end
+
+    def variables(vars)
+            
+      if vars.empty? == true
+        "vars=ALL"
+      else      
+        vars, nvars = check_variables(vars)
+        "vars=#{vars * ":"}"        
+      end    
+    end
+  
+	  # Delete Invalid Variables and keep those valid, if none are valid replace with all
+    def check_variables(vars)
+
+      if vars.kind_of?(Array) == false 
+        vars = [vars]
+      end
+  
+      # Collections of variables and all Possible Variables
+      varOpts = {
+        "WIND" => ["DDVEC", "FG", "FHX", "FHX", "FXX"], 
+        "TEMP" => ["TG", "TN", "TX", "T10N"], 
+        "SUNR" => ["SQ", "SP", "Q"],
+        "PRCP" => ["DR", "RH", "EV24"],
+        "PRES" => ["PG", "PGX", "PGN"],
+        "VICL" => ["VVN", "VVX", "NG"],
+        "MSTR" => ["VVN", "VVX", "NG"],
+        "ALL"  => ["DDVEC", "FHVEC", "FG", "FHX", 
+                   "FHXH", "FHN", "FHNH", "FXX", "FXXH", "TG", "TN", 
+                   "TNH", "TX", "TXH", "T10N", "T10NH", "SQ", "SP", 
+                   "Q", "DR", "RH", "RHX", "RHXH", "EV24", "PG", "PX", 
+                   "PXH", "PN", "PNH", "VVN", "VVNH", "VVX", "VVXH", 
+                   "NG", "UG", "UX", "UXH", "UN", "UNH"]
+      }
+
+        # Drop in invalid vars
+       vars = ( vars & varOpts.to_a.flatten )
+
+       # Only Allow Variable to be selected once, All, or TEMP, not ALL & TEMP
+       # And count the number of selected variables
+    	if vars.include?("ALL") == true or vars.empty? == true
+	      vars = "ALL"
+	      nvars = varOpts["ALL"].count
+    	elsif (vars & varOpts.keys) # check if contains keys
+
+	      x = []
+	      (vars & varOpts.keys).each do |k|
+	        x << varOpts.values_at(k)
+	      end
+	      vars = (vars - x.flatten)
+
+	      nvars = 0
+	      ( vars & varOpts.keys ).each do |v|
+	        nvars += varOpts[v].count
+	      end
+
+	      ( vars - varOpts.keys ).each do |v|
+	        nvars += 1
+	      end      
+    	end
+
+      return vars, nvars
+    end
+    
+    # Parse Response into hashed arrays
+    #Other elements
+    #varlist = res[(7+nstn)..(6 + nstn + nvars)]
+    #colheader = res[(8 + nstn + nvars)]
+    #header = res[0..(9 + nstn + nvars)]
+    def parse_data(response, station_number, vars)
+      # Line Index Numbers
+      nstn = [station_number].flatten.length
+      vars, nvars = check_variables(vars)
+      
+      # Split lines into array
+      response = response.split(/\n/)
+
+      # Get Station Details
+      stations = response[5..(4+nstn)]
+      stations = stations.join.tr("\t", "\s")
+      stations = stations.tr("#", "")
+      stations = stations.tr(":", "")
+      stations = CSV.parse( stations, {:col_sep => "\s"} )
+      stations = stations.map {|row| row.map {|cell| cell.to_s } }
+      st_header = [:station_code, :lng, :lat, :elev, :name]
+      stations = stations.map {|row| Hash[*st_header.zip(row).flatten] }
+
+      # Get Variable Details
+      varlist = response[(7+nstn)..(6 + nstn + nvars)]
+      varlist = varlist.join
+      varlist = varlist.gsub(/# /, "")
+      varlist = varlist.gsub(/\s{2,}/, "")
+      varlist = varlist.gsub(/;/, "\r")
+      varlist = CSV.parse( varlist, {:col_sep => "= "} )
+      vr_header = [:var, :description]
+      varlist = varlist.map {|row| Hash[*vr_header.zip(row).flatten] }
+
+      # Get and clean data
+      response = response[(8 + nstn + nvars)..response.length]
+      response[0] = response[0].sub(/\r/, "")
+      response = response.join.tr("\s+", "")
+      response = response.tr("#", "")
+
+      # Parse into array and then hash with var name
+      response = CSV.parse(response, {:skip_blanks => true})
+      header = response.shift.map {|i| i.to_s.intern }
+      string_data = response.map {|row| row.map {|cell| cell.to_s } }
+      data = string_data.map {|row| Hash[*header.zip(row).flatten] }
+
+      return {:stations => stations, :variables => varlist, :data => data}
+    end
+  end # End Private
+  
+  def initialize(station_number, vars = "")
+    #@vars, @nvars = check_variables(vars)
+    #@station_number = [station_number].flatten
+    @station_number = station_number
+    @vars = vars
+  end
+   
+  def parse
+    parse_data(@response, @station_number, @vars)
+  end
+   
+  # gets variables from station or list of stations
+  # with all variables if vars is empty or selected variables passed as an array
+  # data is from begining of current month to current day
+  # Example
+  # station_number = [210, 212]
+  # vars = "TG"
+  # res = KNMIdaily.get_station( station_number, vars )
+  # output {:stations => {:}}
+  def get_station
+    query = station(@station_number) + "&" + variables(@vars)
+    puts query 
+    @response = get("", { :query => query } )
+  end
+  
+  # gets variables from station or list of stations
+  # with all variables if vars is empty or selected variables passed as an array
+  # data is from start date to current
+  def self.get_station_start_to_current(station_number, start, vars = "")
+    query = station(station_number) + "&" + start_date(start) + "&" + variables(vars)
+    puts query
+    res = get("", { :query => query } )
+    res = {:station_number => station_number, :vars => vars, :output => res}
+  end
+  
+  # gets variables from station or list of stations
+  # with all variables if vars is empty or selected variables passed as an array
+  # data is from start date to end date
+  def self.get_station_range(station_number, _start, _end, vars = "")
+    query = station(station_number) + "&" + start_date(_start) + "&" + end_date(_end) + "&" + variables(vars)
+    puts query
+    res = get("", { :query => query } )
+    res = {:station_number => station_number, :vars => vars, :output => res}
+  end
+  
+  # gets variables from station or list of stations
+  # with all variables if vars is empty or selected variables passed as an array
+  # seasonal data is from start date to end date 
+  # by selected month and day within each year
+  def self.get_seasonal(station_number, _start, _end, vars = "")
+    query = station(station_number) + "&" + "inseason=true" + "&" + start_date(_start) + "&" + end_date(_end) + "&" + variables(vars)
+    puts query
+    res = get("", { :query => query } )
+    res = {:station_number => station_number, :vars => vars, :output => res}
+  end
+  
+  def self.to_csv(filename, response)
+    CSV.open(filename, "wb") do |csv|
+      response[:data]
+      csv << response[:data][0].keys
+      response[(1..(response[:data].length - 1))].each do |line|
+        csv << line.values
+      end
+    end
+  end
+
+end
---
+++ scratch.rb
@@ -0,0 +1,227 @@
+# info
+# http://www.knmi.nl/klimatologie/uurgegevens/scriptxs-nl.html#uur
+# Daily Data
+# http://www.knmi.nl/climatology/daily_data/getdata_day.cgi 
+# http://www.knmi.nl/klimatologie/daggegevens/getdata_dag.cgi 
+
+# Hourly Data
+# http://www.knmi.nl/klimatologie/uurgegevens/getdata_uur.cgi?stns=235 
+
+# Precip Data
+#http://www.knmi.nl/klimatologie/uurgegevens/getdata_uur.cgi 
+# http://www.knmi.nl/climatology/rainfall_data/getdata_rr.cgi
+
+# YAML maker
+require 'YAML'
+require 'CSV'
+
+csv_data = CSV.read "./data/knmi_daily_data_key.csv"
+headers = csv_data.shift.map {|i| i.to_s }
+string_data = csv_data.map {|row| row.map {|cell| cell.to_s } }
+array_of_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
+
+yml = array_of_hashes.to_yaml
+File.open("./data/knmi_daily_data_key.yml", 'w') {|f| f.write(yml) }
+
+# YAML maker
+require 'YAML'
+require 'CSV'
+
+csv_data = CSV.read "./data/knmi_hourly_data_key.csv"
+headers = csv_data.shift.map {|i| i.to_s }
+string_data = csv_data.map {|row| row.map {|cell| cell.to_s } }
+array_of_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
+
+yml = array_of_hashes.to_yaml
+File.open("./data/knmi_hourly_data_key.yml", 'w') {|f| f.write(yml) }
+
+
+# KNMI Station MetaData ################################################
+require 'open-uri'
+require 'nokogiri'
+
+current_stations = [210,225,235,240,242,249,251,257,260,265,267,269,
+  270,273,275,277,278,279,280,283,286,290,310,319,323,330,340,
+  344,348,350,356,370,375,377,380,391]
+  
+
+
+
+res = KNMI.get_station(235, "TX")
+x= res.parse
+
+stations = res[:stations]
+stations.each do |station|
+  station.merge!({
+    :photo => 'http://www.knmi.nl/klimatologie/metadata/' + station[:station_code] + '_' + station[:name].downcase + '_big.jpg',
+    :map => 'http://www.knmi.nl/klimatologie/metadata/stn_' + station[:name] + '.gif',
+    :web => 'http://www.knmi.nl/klimatologie/metadata/valkenburg' + station[:name].downcase + '.html'
+  })
+end
+
+
+doc = Nokogiri::HTML(open('http://www.knmi.nl/klimatologie/metadata/' + station.downcase + '.html'))
+
+rows = doc.css('table.onelinetable')
+
+x = [] 
+rows.search('table').each do |row|
+  properties = row.search('text()').collect {|text| text.to_s}
+  x << properties
+end
+
+d = x.flatten.map { |x| x.strip }.delete_if { |x| x.empty? }
+
+ind = []
+['Temperatuurmetingen', 'Neerslagmetingen', 
+  'Luchtdrukmetingen', 'Windmetingen'].each { |e| ind << d.index { |x| x[/#{e}/]}}
+ind << d.index(d[-1])
+
+data = {}
+["temperature", "precipitation", "pressure", "wind"].each_with_index do |m, index|
+  dat = d[(ind[index] + 2)..(ind[index + 1] - 1)]
+  epoch = dat.values_at( * dat.each_index.select { |i| i.even?} )
+  epoch.map! { |e| e.split(" - ") } 
+  instrument = dat.values_at( * dat.each_index.select { |i| i.odd?} )
+  
+  ts = []
+  epoch.each_with_index do |e, index|
+    ts << {
+      :epoch => index,
+      :start => e[0],
+      :end => e[1],
+      :instrument => instrument[index]
+    }
+  end
+  
+  data.merge!({m.intern => ts})
+end
+
+
+
+
+x = {
+  :epoch => "Present", 
+  :start => epoch.last[0], 
+  :end => "Present", 
+  :instrument => instrument.last
+}
+
+
+
+
+
+station_ind = d.index { |x| x[/Valkenburg/] }
+temp_ind = d.index("Temperatuurmetingen:")
+precip_ind = d.index("Neerslagmetingen:")
+press_ind = d.index("Luchtdrukmetingen:")
+wind_ind = d.index("Windmetingen:")
+
+station = d[(station_ind + 1)..(temp_ind - 1)]
+temp = d[(temp_ind + 2)..(precip_ind - 1)]
+precip = d[(precip_ind + 2)..(press_ind - 1)]
+press = d[(press_ind + 2)..(wind_ind - 1)]
+wind = d[(wind_ind + 2)..(d.length)]
+
+
+epoch = temp.values_at( * temp.each_index.select { |i| i.even?} )
+epoch.map! { |e| e.split(" - ") } 
+instrument = temp.values_at( * temp.each_index.select { |i| i.odd?} )
+
+x = {
+  :epoch => "Present", 
+  :start => epoch.last[0], 
+  :end => "Present", 
+  :instrument => instrument.last
+  }
+
+ind = (0..temp.index(temp.last)).step(2)
+temp[ind]
+temp[0..-1].step(2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Backup
+x = stations.to_yaml
+File.open("./data/current_stations.txt", 'w') {|f| f.write(x) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+x = []
+stations.each do |station|
+  if station[:name].nil? == false
+    x << station
+  end
+end
+
+x = []
+stations.each_with_index do |station|
+  x << station[:name]
+end
+
+
+stations.each do |s|
+  code << s[:station_code]
+  lat << s[:lat]
+  lng << s[:lng]
+  elev << s[:elev]
+  name << s[:name]
+end
+
+
+
+# Get all Stations
+res = KNMI.get_station([(1..600).to_a], "TX")
+
+stations = res[:stations]
+
+# Get Index of valid stations
+x = {}
+["station_code", "lat", "lng", "elev", "name" ].each do |e|
+  x.merge!({e.intern => []})
+
+  stations.each_with_index do |station, index|
+    if station[e.intern].blank? == false
+      x[e.intern] << index
+    end
+  end
+end
+
+x[:lat] == x[:lng]
+x[:lat] == x[:name]
+x[:name] == x[:elev]
+
+index = x[:name]
+
+
+stats = []
+index.each do |i|
+  stats << stations[i]
+end
+  
+File.open("valid_stations", 'w') {|f| f.write(stats) }
+
