5687ec69f3a1ecd65038f2de199853abd06b4de2
bullfight <p.schmitz@gmail.com>
2011-05-17 17:35:52 +0200

    refactored httpservice to output KNMI::HttpService class

--- lib/knmi/httpservice.rb
+++ lib/knmi/httpservice.rb
@@ -4,18 +4,89 @@ module KNMI
     include HTTParty
     
     class << self
+      
+      def get_daily(station_object, parameter_object = "", start_at = nil, end_at = nil, seasonal = false)
+        query = [station(station_object),  parameters(parameter_object), 
+                 start_date(start_at)[0..13], end_date(end_at)[0..11],  # select YYYYMMDD (drops hour term)
+                 seasonal(seasonal)].compact
+        result = get('http://www.knmi.nl/climatology/daily_data/getdata_day.cgi', { :query => "#{query * "&"}" } )
+        return new({"query" => query, "result" => result})
+      end
+  
+      def get_hourly(station_number, params = "", start_at = nil, end_at = nil, seasonal = false)
+        query = [station(station_number),  parameters(params), 
+                 start_date(start_at), end_date(end_at), 
+                 seasonal(seasonal)].compact
+        result = get('http://www.knmi.nl/klimatologie/uurgegevens/getdata_uur.cgi', { :query => "#{query * "&"}" } )
+        return new({"query" => query, "result" => result})
+      end
+        
+      private
     
+      # Generates string for station 
+      def station(station_object) 
+        "stns=#{station_object.id}"
+      end
+    
+      # pulls parameter names from parameter object defaults to All
+      def parameters(parameter_object)
+        if parameter_object.empty?
+          "vars=ALL"
+        elsif parameter_object.kind_of?(Array)
+          vars = []
+          parameter_object.each { |p| vars << p.parameter}                    
+          "vars=#{vars * ":"}"
+        else
+          "vars=#{parameter_object.parameter}"
+        end    
+      end      
+  
+      # Select Data seasonally? between selected dates for each year.
+      def seasonal(bool)
+        if bool == false
+          nil
+        else
+          "inseason=true"
+        end
+      end
+  
+      # YYYYMMDDHH Default is previous day
+      def start_date(start_at)
+        if start_at.nil?
+          t = Time.now
+          t = Time.utc(t.year, t.month, t.day)
+          t = t - 24 * 60 * 60          
+          "start=#{t.year}#{t.strftime("%m")}#{t.day}#{t.strftime("%H")}"
+        elsif start_at.kind_of?(Time)
+          t = start_at
+          "start=#{t.year}#{t.strftime("%m")}#{t.day}#{t.strftime("%H")}"
+        end
+      end
+  
+      # YYYYMMDDHH Default is current day
+      def end_date(end_at)
+        if end_at.nil?
+          t = Time.now
+          "end=#{t.year}#{t.strftime("%m")}#{t.day}#{t.strftime("%H")}"
+        elsif end_at.kind_of?(Time)
+          t = end_at
+          "end=#{t.year}#{t.strftime("%m")}#{t.day}#{t.strftime("%H")}"
+        end
+      end
+    
+    end
+  
     #
     # Returns query string used in HTTP get request
     # KNMI::HttpService.get_daily(210) #=> @ query =>[ "stns=210", "vars=ALL", "start=2011051500", "end=2011051613"]
     attr_reader :query
-    
+  
     #
     # Returns result of HTTP get request
     # This is a text file with station and variable metadata as well as a csv of recorded values
-    
+  
     # Example Response
-    
+  
     ## THESE DATA CAN BE USED FREELY PROVIDED THAT THE FOLLOWING SOURCE IS ACKNOWLEDGED:
     ## ROYAL NETHERLANDS METEOROLOGICAL INSTITUTE
     ##
@@ -35,74 +106,11 @@ module KNMI
     #  235,19700102,   30,   75,    8,   14,   21,    2
     #  235,19700103,   60,   70,    5,    2,    5,    3    
     attr_reader :result
-        
-    def get_daily(station_object, parameter_object = "", start_at = nil, end_at = nil, seasonal = false)
-      @query = [station(station_object),  parameters(parameter_object), 
-               start_date(start_at)[0..13], end_date(end_at)[0..11],  # select YYYYMMDD (drops hour term)
-               seasonal(seasonal)].compact
-      @result = get('http://www.knmi.nl/climatology/daily_data/getdata_day.cgi', { :query => "#{query * "&"}" } )
-    end
   
-    def get_hourly(station_number, params = "", start_at = nil, end_at = nil, seasonal = false)
-      @query = [station(station_number),  parameters(params), 
-               start_date(start_at), end_date(end_at), 
-               seasonal(seasonal)].compact
-      @result = get('http://www.knmi.nl/klimatologie/uurgegevens/getdata_uur.cgi', { :query => "#{query * "&"}" } )
-    end
-        
-      private :new
-    
-      # Generates string for station 
-      def station(station_object) 
-        "stns=#{station_object.id}"
+    def initialize(properties)
+      @query, @result = %w(query result).map do |p|
+        properties[p]
       end
-      
-      # pulls parameter names from parameter object defaults to All
-      def parameters(parameter_object)
-        if parameter_object.empty?
-          "vars=ALL"
-        elsif parameter_object.kind_of?(Array)
-          vars = []
-          parameter_object.each { |p| vars << p.parameter}                    
-          "vars=#{vars * ":"}"
-        else
-          "vars=#{parameter_object.parameter}"
-        end    
-      end      
-    
-      # Select Data seasonally? between selected dates for each year.
-      def seasonal(bool)
-        if bool == false
-          nil
-        else
-          "inseason=true"
-        end
-      end
-    
-      # YYYYMMDDHH Default is previous day
-      def start_date(start_at)
-        if start_at.nil?
-          t = Time.now
-          t = Time.utc(t.year, t.month, t.day)
-          t = t - 24 * 60 * 60          
-          "start=#{t.year}#{t.strftime("%m")}#{t.day}#{t.strftime("%H")}"
-        elsif start_at.kind_of?(Time)
-          t = start_at
-          "start=#{t.year}#{t.strftime("%m")}#{t.day}#{t.strftime("%H")}"
-        end
-      end
-    
-      # YYYYMMDDHH Default is current day
-      def end_date(end_at)
-        if end_at.nil?
-          t = Time.now
-          "end=#{t.year}#{t.strftime("%m")}#{t.day}#{t.strftime("%H")}"
-        elsif end_at.kind_of?(Time)
-          t = end_at
-          "end=#{t.year}#{t.strftime("%m")}#{t.day}#{t.strftime("%H")}"
-        end
-      end
-    
     end
   end
 end
